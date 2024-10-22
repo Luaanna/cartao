82452ba599f33c02ed0736846c8a4d008593e590
Luaanna <142031681+Luaanna@users.noreply.github.com>
2024-10-22 11:02:55 -0300

    Add files via upload

---
+++ bkpCart-oAlura-main/Cartão3Trimestre/perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+ //categoria,
+'programação'
+//pergunta,
+'o que é JavaScript?'
+//resposta,
+'o JavaScript é uma linguagem de programação'
+   )
+   criaCartao(
+    //categoria,
+    'programação'
+    //pergunta,
+    'o que é JavaScript?'
+    //resposta,
+    'o JavaScript é uma linguagem de programação'
+       )
+        criaCartao(
+        //categoria,
+        'programação'
+        //pergunta,
+        'o que é JavaScript?'
+        //resposta,
+        'o JavaScript é uma linguagem de programação'
+           )
