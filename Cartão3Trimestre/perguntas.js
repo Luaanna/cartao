289fb3c1f10fbaabeba4849a9a17466eb54cec65
Luaanna <142031681+Luaanna@users.noreply.github.com>
2024-10-29 11:00:58 -0300

    Add files via upload

---
+++ Cartão3Trimestre/perguntas.js
@@ -0,0 +1,31 @@
+criaCartao(
+ //categoria,
+'programação',
+//pergunta,
+'o que é JavaScript?',
+//resposta,
+'o JavaScript é uma linguagem de programação'
+   )
+   criaCartao(
+    //categoria,
+    'programação',
+    //pergunta,
+    'o que é JavaScript?',
+    //resposta,
+    'o JavaScript é uma linguagem de programação'
+       )
+        criaCartao(
+        //categoria,
+        'programação',
+        //pergunta,
+        'o que é JavaScript?',
+        //resposta,
+        'o JavaScript é uma linguagem de programação'
+           )
+
+   criaCartao(
+
+      'literatura',
+      'qual é o nome da saga de game of thrones',
+      'o nome é a canção gelo e fogo',
+   )
